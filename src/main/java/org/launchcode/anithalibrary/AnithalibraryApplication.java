3e910f8b96404366f3c736311568566d78a2ce3d
anicraj456 <anicraj123@gmail.com>
2024-03-19 17:39:39 -0500

    spring intial

---
+++ src/main/java/org/launchcode/anithalibrary/AnithalibraryApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.anithalibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnithalibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnithalibraryApplication.class, args);
+	}
+
+}
