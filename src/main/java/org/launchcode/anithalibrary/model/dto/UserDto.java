d8330c6dbae60f2af7df0189a935eac19cdba699
anicraj456 <anicraj123@gmail.com>
2024-08-13 20:02:50 -0500

    updated test

--- src/main/java/org/launchcode/anithalibrary/model/dto/UserDto.java
+++ src/main/java/org/launchcode/anithalibrary/model/dto/UserDto.java
@@ -4,19 +4,19 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 
 public class UserDto {
-        private Long id;
-        @NotEmpty
-        private String firstName;
-        @NotEmpty
-        private String lastName;
+    private Long id;
+    @NotEmpty
+    private String firstName;
+    @NotEmpty
+    private String lastName;
 
-        @NotEmpty
-        private String userName;
-        @NotEmpty(message = "Email should not be empty")
-        @Email
-        private String email;
-        @NotEmpty(message = "Password should not be empty")
-        private String password;
+    @NotEmpty
+    private String userName;
+    @NotEmpty(message = "Email should not be empty")
+    @Email
+    private String email;
+    @NotEmpty(message = "Password should not be empty")
+    private String password;
 
 
     public UserDto(Long id, String firstName, String lastName, String userName, String email, String password) {
@@ -79,4 +79,3 @@ public class UserDto {
         this.userName = userName;
     }
 }
-
